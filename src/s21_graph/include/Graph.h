9adedf077faa29fd1aaf8184eaf1a2eafe4521e9
ltowelie <ltowelie@student.21-school.ru>
2023-01-27 20:45:50 +0300

    added << operator and writing to file with .dot or .gv format

--- src/s21_graph/include/Graph.h
+++ src/s21_graph/include/Graph.h
@@ -4,21 +4,30 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <string>
+#include <charconv>
+#include <sstream>
+#include <fstream>
 
 class Graph
 {
 public:
 	Graph(unsigned **matrix, size_t size);
+
 	Graph(const Graph &other);
+
 	~Graph();
 
 	Graph &operator=(const Graph &other);
 
-	void loadGraphFromFile(const std::string& filename);
-	void exportGraphToDot(const std::string& filename);
-	[[nodiscard]] size_t size() const;
+	void loadGraphFromFile(const std::string &filename);
 
-	class WrongMatrixException: public std::exception
+	void exportGraphToDot(const std::string &filename);
+
+	[[nodiscard]] size_t size() const;
+	std::string generateDotString();
+
+	class WrongMatrixException : public std::exception
 	{
 	public:
 		[[nodiscard]] const char *what() const noexcept override;
@@ -27,13 +36,16 @@ public:
 private:
 	Graph();
 
-	unsigned** a_matrix;
+	unsigned **a_matrix;
 	size_t a_matrix_size;
+	bool weighted;
 
-	void set_a_matrix(unsigned ** matrix, size_t size);
-	static void check_matrix(unsigned int *const *matrix, size_t size) ;
+	void set_a_matrix(unsigned **matrix, size_t size);
+
+	static void check_matrix(unsigned int *const *matrix, size_t size);
+
 };
 
-std::ostream	&operator<<(std::ostream &o, Graph *a);
+std::ostream &operator<<(std::ostream &o, Graph *a);
 
 #endif
--- src/s21_graph/src/Graph.cpp
+++ src/s21_graph/src/Graph.cpp
@@ -1,10 +1,12 @@
 #include "Graph.h"
+#include <filesystem>
 
-Graph::Graph(): a_matrix(nullptr), a_matrix_size(0)
+Graph::Graph() : a_matrix(nullptr), a_matrix_size(0), weighted(false)
 {
 }
 
-Graph::Graph(unsigned **matrix, size_t size): a_matrix(nullptr), a_matrix_size(0)
+Graph::Graph(unsigned **matrix, size_t size) :
+		a_matrix(nullptr), a_matrix_size(0), weighted(false)
 {
 	set_a_matrix(matrix, size);
 }
@@ -16,7 +18,8 @@ Graph::~Graph()
 	delete[] a_matrix;
 }
 
-Graph::Graph(const Graph &other): a_matrix(nullptr), a_matrix_size(0)
+Graph::Graph(const Graph &other) :
+a_matrix(nullptr), a_matrix_size(0), weighted(false)
 {
 	set_a_matrix(other.a_matrix, other.a_matrix_size);
 }
@@ -41,32 +44,68 @@ void Graph::set_a_matrix(unsigned **matrix, size_t size)
 		delete[] a_matrix[i];
 	delete[] this->a_matrix;
 	a_matrix_size = size;
-	a_matrix = new unsigned*[size];
+	a_matrix = new unsigned *[size];
 	for (unsigned i = 0; i < size; ++i)
 	{
 		a_matrix[i] = new unsigned[size];
 		for (unsigned j = 0; j < size; ++j)
 		{
 			a_matrix[i][j] = matrix[i][j];
+			if (!weighted && a_matrix[i][j] > 1)
+			{
+				weighted = true;
+			}
 		}
 	}
 }
 
-void dfs(unsigned start, unsigned *const *matrix, std::vector<bool> *used, size_t size) {
+void dfs(unsigned start, unsigned *const *matrix, std::vector<bool> *used,
+		 size_t size)
+{
 
 	std::stack<unsigned> stack;
 
 	stack.push(start);
-	while (! stack.empty()) {
+	while (!stack.empty())
+	{
 		auto current = stack.top();
 		stack.pop();
 		(*used)[current] = true;
-		for (unsigned i = 0; i < size; ++i) {
-			if (matrix[current][i] == 1 && ! (*used)[i]) {
+		for (unsigned i = 0; i < size; ++i)
+		{
+			if (matrix[current][i] != 0 && !(*used)[i])
+			{
 				stack.push(i);
 			}
 		}
 	}
+}
+
+std::string *generate_node_names(unsigned count)
+{
+	auto names = new std::string[count];
+	int base_len = -2;
+	char char_idx = 'a';
+	std::string s;
+	for (unsigned i = 0; i < count; ++i)
+	{
+		if (i % 26 == 0)
+		{
+			char_idx = 'a';
+			base_len++;
+		}
+		char c = char_idx++;
+		if (i > 25)
+		{
+			s = {c, char(base_len + 48)};
+		} else
+		{
+			s = {c};
+		}
+
+		names[i] = s;
+	}
+	return names;
 }
 
 void Graph::check_matrix(unsigned *const *matrix, size_t size)
@@ -86,34 +125,82 @@ void Graph::check_matrix(unsigned *const *matrix, size_t size)
 				throw WrongMatrixException();
 			cons += matrix[i][j];
 		}
-		if (cons == 0) {
+		if (cons == 0)
+		{
 			throw WrongMatrixException();
 		}
-		if (! used[i]) {
+		if (!used[i])
+		{
 			dfs(i, matrix, &used, size);
 			res++;
 		}
-		if (res > 1) {
+		if (res > 1)
+		{
 			throw WrongMatrixException();
 		}
 
 	}
 }
 
-void Graph::loadGraphFromFile(const std::string& filename)
+void Graph::loadGraphFromFile(const std::string &filename)
 {
 	std::cout << filename;
 }
 
-void Graph::exportGraphToDot(const std::string& filename)
+void Graph::exportGraphToDot(const std::string &filename)
 {
-	std::cout << filename;
+	std::ofstream ofs;
+
+	std::filesystem::path filepath = filename;
+	if (filepath.extension() != ".gv" && filepath.extension() != ".dot")
+	{
+		throw std::invalid_argument("Wrong extension of file");
+	}
+
+	ofs.open(filename);
+	if (! ofs.is_open())
+	{
+		std::cerr << "Can't open file: " << filename << std::endl;
+	}
+	else
+	{
+		ofs << this;
+		ofs.close();
+	}
 }
 
-std::ostream	&operator<<(std::ostream &os, Graph *b)
+std::string Graph::generateDotString()
 {
-	// todo Print values of matrix in dot format
-	os << b->size();
+	std::stringstream res;
+	std::string pp_ident = "    ";
+	auto nn = generate_node_names(this->a_matrix_size);
+	unsigned current_iter = 0;
+
+	res << "graph {\n";
+	for (unsigned i = 0; i < this->a_matrix_size; ++i)
+	{
+		for (unsigned j = i; j < this->a_matrix_size; ++j)
+		{
+			if (this->a_matrix[i][j] == 0)
+			{
+				continue;
+			}
+			res << pp_ident << nn[i] << " -- " << nn[j];
+			if (weighted) {
+				res << " [weight=" << a_matrix[i][j] << "]";
+			}
+			res << "\n";
+			current_iter++;
+		}
+	}
+	res << "}\n";
+	delete []nn;
+	return res.str();
+}
+
+std::ostream &operator<<(std::ostream &os, Graph *b)
+{
+	os << b->generateDotString();
 	return (os);
 }
 
