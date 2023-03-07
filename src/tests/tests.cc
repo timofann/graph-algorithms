cf3bdcd8d7c7fbfbec3d8c3c833997d3eef50b40
dedelmir <dedelmir@student.21-school.ru>
2023-03-07 04:38:30 +0300

    working on tests

--- src/tests/tests.cc
+++ src/tests/tests.cc
@@ -11,9 +11,8 @@ TEST(graph, loadStandardGraphFromFile) {
 }
 
 TEST(graph, loadElementaryGraphFromFile) {
-    s21::Graph graph = s21::Graph::loadGraphFromFile("tests/graphs/4.txt");
-    EXPECT_EQ(graph.size(), 1);
-} // wrong matrix, но граф может состоять из одной вершины
+    EXPECT_ANY_THROW(s21::Graph graph = s21::Graph::loadGraphFromFile("tests/bad_graphs/3.txt"));
+} // wrong matrix, но граф может состоять из одной вершины ! ошиблась, по сабджу не может (эту проверку лучше делать в конструкторе, а не в парсере)
 
 TEST(graph, loadSmallGraphFromFile) {
     s21::Graph graph = s21::Graph::loadGraphFromFile("tests/graphs/2.txt");
@@ -26,10 +25,19 @@ TEST(graph, loadNonExistGraphFromFile) {
 
 TEST(graph, loadNullGraphFromFile) {
     EXPECT_ANY_THROW(s21::Graph graph = s21::Graph::loadGraphFromFile("tests/bad_graphs/1.txt"));
-} // создается нулевой граф, не соответствует сабджекту
+} // создается нулевой граф, не соответствует сабджекту (ошибка должна обрабатываться в конструкторе)
 
 TEST(graph, loadDirectedGraphFromFile) {
     EXPECT_ANY_THROW(s21::Graph graph = s21::Graph::loadGraphFromFile("tests/bad_graphs/2.txt"));
+}
+
+TEST(graph, loadIncompleteGraphFromFile) {
+    s21::Graph graph = s21::Graph::loadGraphFromFile("tests/graphs/4.txt");
+    EXPECT_EQ(graph.size(), 5);
+}
+
+TEST(graph, loadOneSideDirectedGraphFromFile) {
+    EXPECT_ANY_THROW(s21::Graph graph = s21::Graph::loadGraphFromFile("tests/bad_graphs/4.txt"));
 }
 
 TEST(graph, loadGraphFromEmptyFile) {
@@ -41,10 +49,10 @@ TEST(graph, loadGraphFromNonReadableFile) {
     EXPECT_EQ(graph.size(), 11); // не та ошибка бросается: файл есть, нет прав на чтение
 }
 
-TEST(graph, loadGraphFromFileWithoutRowsNumber) {
-    s21::Graph graph = s21::Graph::loadGraphFromFile("tests/bad_graphs/8.txt");
-    EXPECT_EQ(graph.size(), 3); // вместо ошибки кидается exit
-} //expect Fail test
+//TEST(graph, loadGraphFromFileWithoutRowsNumber) {
+//    s21::Graph graph = s21::Graph::loadGraphFromFile("tests/bad_graphs/8.txt");
+//    EXPECT_EQ(graph.size(), 3); // вместо ошибки кидается exit
+//} //expect Fail test
 
 //TEST(graph, loadGraphFromFileWithInvalidLength) {
 //    s21::Graph graph = s21::Graph::loadGraphFromFile("tests/bad_graphs/9.txt");
@@ -111,25 +119,62 @@ TEST(graph, exportGraphToDot2) {
 /* ------------------------------------------------------------------------------------------------- exportGraphToDot */
 
 
-TEST(graph_algorithms, depthFirstSearchM) {
+/* ----------------------------------------------------------------------------------------------------------- search */
+
+TEST(graph_algorithms, depthFirstSearchStandard) {
     s21::Graph graph = s21::Graph::loadGraphFromFile("tests/graphs/1.txt");
     std::vector<int> res = s21::GraphAlgorithms::depthFirstSearch(graph, 1);
     std::vector<int> vec{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
     EXPECT_EQ(res, vec);
 }
 
-TEST(graph_algorithms, BreadthFirstSearch) {
-    s21::Graph graph = s21::Graph::loadGraphFromFile("tests/graphs/1.txt");
-    std::vector<int> res = s21::GraphAlgorithms::breadthFirstSearch(graph, 1);
-    std::vector<int> vec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+TEST(graph_algorithms, depthFirstSearchSmall) {
+    s21::Graph graph = s21::Graph::loadGraphFromFile("tests/graphs/2.txt");
+    std::vector<int> res = s21::GraphAlgorithms::depthFirstSearch(graph, 2);
+    std::vector<int> vec{ 2, 1 };
     EXPECT_EQ(res, vec);
 }
 
-TEST(graph_algorithms, GetShortestPathBetweenVertices) {
+TEST(graph_algorithms, breadthFirstSearchStandard) {
     s21::Graph graph = s21::Graph::loadGraphFromFile("tests/graphs/1.txt");
-    int res = s21::GraphAlgorithms::getShortestPathBetweenVertices(graph, 1, 11);
-    EXPECT_EQ(res, 18);
+    std::vector<int> res = s21::GraphAlgorithms::breadthFirstSearch(graph, 1);
+    std::vector<int> vec{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+    EXPECT_EQ(res, vec);
 }
+
+TEST(graph_algorithms, breadthFirstSearchSmall) {
+    s21::Graph graph = s21::Graph::loadGraphFromFile("tests/graphs/2.txt");
+    std::vector<int> res = s21::GraphAlgorithms::breadthFirstSearch(graph, 2);
+    std::vector<int> vec{ 2, 1 };
+    EXPECT_EQ(res, vec);
+}
+
+TEST(graph_algorithms, breadthFirstSearchErrorLargeVertex) {
+    s21::Graph graph = s21::Graph::loadGraphFromFile("tests/graphs/2.txt");
+    EXPECT_THROW(s21::GraphAlgorithms::breadthFirstSearch(graph, 3), s21::GraphAlgorithms::GraphAlgorithmsError);
+}
+
+TEST(graph_algorithms, breadthFirstSearchErrorZeroVertex) {
+    s21::Graph graph = s21::Graph::loadGraphFromFile("tests/graphs/2.txt");
+    EXPECT_THROW(s21::GraphAlgorithms::breadthFirstSearch(graph, 0), s21::GraphAlgorithms::GraphAlgorithmsError);
+}
+
+TEST(graph_algorithms, depthFirstSearchStandard) {
+    s21::Graph graph = s21::Graph::loadGraphFromFile("tests/graphs/4.txt");
+    std::vector<int> res = s21::GraphAlgorithms::depthFirstSearch(graph, 2);
+    std::vector<int> vec{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+    EXPECT_EQ(res, vec);
+}
+
+/* ----------------------------------------------------------------------------------------------------------- search */
+
+
+
+//TEST(graph_algorithms, GetShortestPathBetweenVertices) {
+//    s21::Graph graph = s21::Graph::loadGraphFromFile("tests/graphs/1.txt");
+//    int res = s21::GraphAlgorithms::getShortestPathBetweenVertices(graph, 1, 11);
+//    EXPECT_EQ(res, 18);
+//}
 
 //TEST(graph_algorithms, GetShortestPathsBetweenAllVertices) {
 //    s21::Graph graph = s21::Graph::loadGraphFromFile("tests/graphs/3.txt");
@@ -145,11 +190,11 @@ TEST(graph_algorithms, GetShortestPathBetweenVertices) {
 //    EXPECT_TRUE(res.EqMatrix(result));
 //}
 
-TEST(graph_algorithms, AntColonyOptimization) {
-    s21::Graph graph = s21::Graph::loadGraphFromFile("tests/graphs/3.txt");
-    s21::GraphAlgorithms::TsmResult res = s21::GraphAlgorithms::solveTravelingSalesmanProblem(graph);
-    EXPECT_EQ(res.distance, 40);
-}
+//TEST(graph_algorithms, AntColonyOptimization) {
+//    s21::Graph graph = s21::Graph::loadGraphFromFile("tests/graphs/3.txt");
+//    s21::GraphAlgorithms::TsmResult res = s21::GraphAlgorithms::solveTravelingSalesmanProblem(graph);
+//    EXPECT_EQ(res.distance, 40);
+//}
 
 
 
