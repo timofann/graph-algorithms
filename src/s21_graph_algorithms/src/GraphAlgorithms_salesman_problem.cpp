6d50367a89990d7cc362deccab15198d711b7f28
dedelmir <dedelmir@student.21-school.ru>
2023-03-11 02:44:09 +0300

    add last city in the sum & return right format

--- src/s21_graph_algorithms/src/GraphAlgorithms_salesman_problem.cpp
+++ src/s21_graph_algorithms/src/GraphAlgorithms_salesman_problem.cpp
@@ -2,8 +2,6 @@
 
 using namespace s21;
 
-
- //First version
 #define Q_VAL 10000.0 // количество фермента, которое муравей может распределить по пути
 #define ANTS_NUM 20 // количество муравьев в каждом городе
 #define ALPHA 1.0 // степень влияния количества ферромона на выбор пути (0 - остальные муравьи безразличны, обычный жадный алгоритм: хватай, что поближе)
@@ -17,12 +15,6 @@ struct Ant {
 	int current_city;
 	std::size_t length;
 };
-
-//static std::vector<int>
-//euler_graph_check(Graph &graph) {
-//	std::vector<int> cities_to_start_from;
-//	return cities_to_start_from;
-//}
 
 static void
 restart_ants(std::vector<Ant>& ants) {
@@ -120,8 +112,8 @@ update_pheromone_lvl_rho(const Graph& graph, std::vector<std::vector<double>>& p
 static void
 finish_path(const Graph& graph, Ant& ant) {
     if (graph[ant.traversed.back()][ant.traversed.front()]) {
-        ant.traversed.push_back(ant.traversed.front());
         ant.length += graph[ant.traversed.back()][ant.traversed.front()];
+        ant.traversed.push_back(ant.traversed.front());
     }
 }
 
@@ -216,13 +208,14 @@ collect_path(const Graph& graph, std::vector<Ant>& elite_ants) {
     }
     if (best_ant == NULL)
         throw GraphAlgorithms::GraphAlgorithmsError("Ants couldn't find the shortest path.");
+    for (auto city = best_ant->traversed.begin(); city != best_ant->traversed.end(); city++)
+        *city = *city + 1;
     return GraphAlgorithms::TsmResult{best_ant->traversed, (double)best_ant->length};
 }
 
 
 GraphAlgorithms::TsmResult GraphAlgorithms::
 solveTravelingSalesmanProblem(Graph &graph) {
-//	std::vector<int> cities_to_start_from = euler_graph_check(graph);
 	std::vector<Ant> ants = std::vector<Ant>(graph.size(),
 											 Ant{std::vector<bool>(graph.size(), false),
 												 std::vector<int>(), 0, 0});
@@ -235,198 +228,3 @@ solveTravelingSalesmanProblem(Graph &graph) {
 
     return collect_path(graph, ants);
 }
- //First version end
-
-
-
-
-
-
-
-
-
-/* Second attempt
-
-#define KLAPCOUNTER 30
-#define kAlpha 1.0
-#define kBeta 1.0
-#define kQ 1.0
-#define kRemainigPheromone 0.75
-
-struct Ant {
-    int number;
-    std::vector<bool> is_traversed;
-    std::vector<int> traversed;
-    int current_city;
-    std::size_t length;
-};
-
-//ACO::ACO(Graph& graph) : distance_(graph.GetGraph()) {
-//  num_cities_ = num_ants_ = graph.GetSize();
-//}
-
-static void
-update_ant(const Graph &graph, Ant& ant, int path) {
-    ant.length += graph[ant.current_city][path];
-    ant.traversed.push_back(path);
-    ant.is_traversed[path] = true;
-    ant.current_city = path;
-}
-
-static
-void InitPheromones(const s21::Graph& graph, std::vector<std::vector<double>>& pheromones) {
-    for (int i = 0; i < (int)graph.size(); ++i) {
-        for (int j = 0; j < (int)graph.size(); ++j) {
-            if (i != j) pheromones[i][j] = 0.5;
-        }
-    }
-}
-
-static std::vector<int>
-get_available_paths(const Graph &graph, const Ant& ant) {
-    std::vector<int> available_paths;
-    for (int i = 0; i < (int)graph.size(); ++i)
-        if (graph[ant.current_city][i] && ant.is_traversed[i] == false)
-            available_paths.push_back(i);
-    return available_paths;
-}
-
-static void
-finish_path(const Graph& graph, Ant& ant) {
-    if (graph[ant.traversed.back()][ant.traversed.front()]) {
-        ant.traversed.push_back(ant.traversed.front());
-        ant.length += graph[ant.traversed.back()][ant.traversed.front()];
-    }
-}
-
-static int
-choose_path(const Graph &graph, const std::vector<std::vector<double>>& pheromone_lvl,
-            const std::vector<int>& available_paths, const Ant& ant) {
-    std::vector<double> probabilities = std::vector(available_paths.size(), 0.0);
-    double sum = 0;
-    for (auto city = available_paths.begin(); city != available_paths.end(); city++)
-        sum += pow(1.0 / graph[ant.current_city][*city], kBeta) * pow(pheromone_lvl[ant.current_city][*city], kAlpha);
-    for (int i = 0; i < (int)available_paths.size(); i++)
-        probabilities[i] = 100 * pow(1.0 / graph[ant.current_city][available_paths[i]], kBeta) *
-                           pow(pheromone_lvl[ant.current_city][available_paths[i]], kAlpha) / sum;
-    double random_choice = std::rand() % 100;
-
-//    for (auto p = probabilities.begin(); p != probabilities.end(); ++p)
-//        printf("%6.2f ", *p);
-//    printf("random %f ", random_choice); //debug
-
-    double probability_integral = 0;
-    int path = ant.current_city;
-    for (int i = 0; i < (int)available_paths.size(); i++) {
-        probability_integral += probabilities[i];
-        if (probability_integral >= random_choice && path == ant.current_city)
-            path = available_paths[i];
-    }
-    if (path == ant.current_city) // на случай погрешностей double
-        path = available_paths.back();
-//    printf("path %d\n", path); //debug
-    return path;
-}
-
-static void
-StartCircle(const Graph& graph, std::vector<Ant>& ants, std::vector<std::vector<double>>& pheromone_lvl) {
-    std::vector<int> available_paths;
-    int path;
-
-    for (auto ant = ants.begin(); ant != ants.end(); ant++) {
-        available_paths = get_available_paths(graph, *ant);
-        while (available_paths.size() > 0) {
-            path = choose_path(graph, pheromone_lvl, available_paths, *ant);
-            update_ant(graph, *ant, path);
-            available_paths = get_available_paths(graph, *ant);
-        }
-//    for (auto city = ant->traversed.begin(); city != ant->traversed.end(); ++city)
-//        printf("%3d ", *city);
-//    printf("\n"); //debug
-        finish_path(graph, *ant);
-    }
-}
-
-
-static
-void InitColony(std::vector<Ant>& ants) {
-    for (int i = 0; i < (int)ants.size(); ++i) {
-        ants[i].number = i;
-        ants[i].current_city = i;
-        ants[i].traversed.push_back(i);
-        ants[i].is_traversed[i] = true;
-        ants[i].length = 0;
-    }
-}
-
-static void
-update_pheromones_rho(const Graph& graph, std::vector<std::vector<double>>& pheromones) {
-    for (int i = 0; i < (int)graph.size(); ++i)
-        for (int j = 0; j < (int)graph.size(); ++j)
-            pheromones[i][j] *= kRemainigPheromone;
-}
-
-
-static void
-update_pheromones_ant(const Graph& graph, const Ant& ant, std::vector<std::vector<double>>& pheromones) {
-    double delta;
-    if (ant.traversed.size() == graph.size() + 1) {
-        delta = kQ / (double)ant.length;
-        for (int j = 0; j < (int)graph.size(); ++j) {
-            pheromones[ant.traversed[j]][ant.traversed[j + 1]] += delta;
-            pheromones[ant.traversed[j + 1]][ant.traversed[j]] += delta;
-        }
-    }
-}
-
-static
-void UpdatePheromones(const Graph& graph, const std::vector<Ant>& ants, std::vector<std::vector<double>>& pheromones) {
-    update_pheromones_rho(graph, pheromones);
-    for (auto ant = ants.begin(); ant < ants.end(); ant++) {
-        update_pheromones_ant(graph, *ant, pheromones);
-    }
-}
-
-static
-void UpdateResult(int& lapcounter, const Graph& graph, std::vector<Ant>& ants, Ant* best_ant) {
-    for (auto ant = ants.begin(); ant != ants.end(); ++ant) {
-        if (ant->traversed.size() == graph.size()
-            && graph[ant->traversed.back()][ant->traversed.front()]
-            && (best_ant == NULL || ant->length < best_ant->length)) {
-            if (best_ant)
-                *best_ant = *ant;
-            else
-                best_ant = new Ant(*ant);
-            lapcounter = 0;
-            printf("FOUND\n");
-        }
-    }
-    InitColony(ants);
-}
-
-GraphAlgorithms::TsmResult GraphAlgorithms::solveTravelingSalesmanProblem(Graph &graph) {
-    std::vector<std::vector<double>> pheromones = std::vector<std::vector<double>>(
-            graph.size(), std::vector<double>(graph.size(), 0.5));
-    std::vector<Ant> ants = std::vector<Ant>(graph.size(),
-                                             Ant{0, std::vector<bool>(graph.size(), false),
-                                                 std::vector<int>(), 0, 0});
-    Ant *best_ant = NULL;
-
-    InitPheromones(graph, pheromones);
-    for (int i = 0; i < KLAPCOUNTER; i++) {
-        InitColony(ants);
-//        for (auto& ant : ants_) {
-//            ant->StartCircle();
-//        }
-        StartCircle(graph, ants, pheromones);
-        UpdatePheromones(graph, ants, pheromones);
-        UpdateResult(i, graph, ants, best_ant);
-    }
-
-    if (best_ant == NULL)
-        throw GraphAlgorithms::GraphAlgorithmsError("Ants can't find path");
-    GraphAlgorithms::TsmResult res = GraphAlgorithms::TsmResult{best_ant->traversed, (double)best_ant->length};
-    delete best_ant;
-    return res;
-}
-*/
